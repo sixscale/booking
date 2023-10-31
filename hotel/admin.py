48358750686b4258004ae8dae99f48143c4a4408
Никита Рябов <gfgfgf354@gmail.com>
2023-10-31 15:00:27 +0300

    переделал админку

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reserve, Room
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('number', 'price', 'number_seats')
+    search_fields = ('number', 'price', 'number_seats')
+
+
+@admin.register(Reserve)
+class ReserveAdmin(admin.ModelAdmin):
+    list_display = ('room_number', 'start_date', 'end_date')
+    search_fields = ('room_number', 'start_date', 'end_date')
+
+    def room_number(self, obj):
+        return obj.room.number
+
+    room_number.short_description = 'номер комнаты'
